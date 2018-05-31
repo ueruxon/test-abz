ed545032daa1ea8862882366914c719f216ba2df
a-kon <kondratyev.work@gmail.com>
2018-05-31 21:13:31 +0300

    index html refresh in HMR

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import bug from './index.html'; // shit for html reload in webpack 4
 
 export default class App extends React.Component {
   render = () => <div>React is Up</div>;
